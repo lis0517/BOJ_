31a06ce82688033111e1e8334edc545993fc1d09
lis0517 <lis0517@naver.com>
2020-01-15 13:56:38 +0900

    20200115

---
+++ n_1000/n_1000/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace n_1000
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string read = Console.ReadLine();
+
+            string[] split = read.Split();
+
+            int a = int.Parse(split[0]);
+            int b = int.Parse(split[1]);
+            Console.WriteLine(a + b);
+        }
+    }
+}
---
+++ n_1002/n_1002/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace n_1002
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            string read = Console.ReadLine();
+
+            int inputLoopCount = int.Parse(read);
+
+            for (int i = 0; i < inputLoopCount; ++i)
+            {
+                string ex = Console.ReadLine();
+
+                string[] ss = ex.Split();
+
+                int x1 = int.Parse(ss[0]);
+                int y1 = int.Parse(ss[1]);
+                int r1 = int.Parse(ss[2]);
+                int x2 = int.Parse(ss[3]);
+                int y2 = int.Parse(ss[4]);
+                int r2 = int.Parse(ss[5]);
+
+                double dst = Math.Sqrt(Math.Pow((x1 - x2), 2) + Math.Pow((y1 - y2), 2));
+
+
+                if (x1 == x2 && y1 == y2)
+                {
+                    if (r1 == r2)
+                        Console.WriteLine(-1);
+                    else
+                        Console.WriteLine(0);
+                }
+                else
+                {
+                    if ((r1 + r2) > dst && Math.Abs(r1 - r2) < dst)
+                    {
+                        Console.WriteLine(2);
+                    }
+                    else if ((r1 + r2) == dst || Math.Abs(r1 - r2) == dst)
+                    {
+                        Console.WriteLine(1);
+                    }
+                    else
+                    {
+                        Console.WriteLine(0);
+                    }
+                }
+            }
+        }
+    }
+}
