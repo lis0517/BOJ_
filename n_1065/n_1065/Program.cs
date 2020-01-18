86e24df5d42ebb9d6ed10c80bf64cbd1527b5dbb
lis0517 <lis0517@naver.com>
2020-01-19 01:00:28 +0900

    20200119

--- n_1065/n_1065/Program.cs
+++ n_1065/n_1065/Program.cs
@@ -5,10 +5,52 @@ namespace n_1065
     class Program
     {
 
+        static bool theNumber(int num)
+        {
+            if (num < 100)
+                return true;
+
+            int length = num.ToString().Length;
+
+            int[] numbers = new int[length];
+
+            int idx = 0;
+            while (num != 0)
+            {
+                numbers[idx] = num % 10;
+                num /= 10;
+                ++idx;
+            }
+
+            int curr = numbers[0] - numbers[1];
+            int prev = curr; 
+
+            for(int i=1; i < length-1; ++i)
+            {
+                curr = numbers[i] - numbers[i + 1];
+
+                if(curr != prev)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string r = Console.ReadLine();
+            int loop = int.Parse(r);
+
+            int count = 0;
+            for(int i = 1; i <= loop; ++i)
+            {
+                if (theNumber(i))
+                    ++count;
+            }
+
+            Console.WriteLine(count);
         }
     }
 }
---
+++ n_11654/n_11654/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace n_11654
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string r = Console.ReadLine();
+
+            char c = r[0];
+
+            //int n = Convert.ToInt32(c);
+            int n = (int)c;
+
+            Console.WriteLine(n);
+        }
+    }
+}
