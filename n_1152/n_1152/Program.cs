eae3a4d4ca12464d95dfa22fd09d1e6c87c20265
lis0517 <lis0517@naver.com>
2020-01-20 01:54:28 +0900

    20200120

---
+++ n_1152/n_1152/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace n_1152
+{
+    class Program
+    {
+        static int checkSpace(string[] s)
+        {
+            int count = 0;
+            for (int i = 0; i < s.Length; ++i)
+            {
+                if (s[i] == "")
+                    ++count;
+            }
+            return count;
+        }
+
+        static void Main(string[] args)
+        {
+            string r = Console.ReadLine();
+            string[] rr = r.Split();
+
+            int spaceCount = checkSpace(rr);
+
+            Console.WriteLine(rr.Length - spaceCount);
+        }
+    }
+}
---
+++ n_2908/n_2908/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace n_2908
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string r = Console.ReadLine();
+            string[] rr = r.Split();
+
+            int[] ans = new int[rr.Length];
+            for(int i=0; i < rr.Length; ++i)
+            {
+                string temp = string.Empty;
+                for (int j = rr[i].Length - 1; j >= 0; --j)
+                {
+                    temp += rr[i][j];
+                }
+                ans[i] = int.Parse(temp);
+            }
+
+            int max = Math.Max(ans[0], ans[1]);
+
+            Console.WriteLine(max);
+        }
+    }
+}
