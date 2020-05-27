4e32b6b9975cf8c9d7296f41ad534008d5623ee1
lis0517 <43354156+lis0517@users.noreply.github.com>
2020-05-27 18:42:42 +0900

    20200527 update

---
+++ apr.2020after/20200525/N_1562/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,36 @@
+﻿#include <stdio.h>
+#pragma warning(disable:4996)
+using namespace std;
+
+int main() {
+	int dp[101][10] = { 0 };
+	int n, sum = 0;
+	scanf("%d", &n);
+	for (int i = 1; i < 10; i++) {
+		dp[1][i] = 1;
+		if (i == 9) {
+			dp[2][i] = 1;
+		}
+		else {
+			dp[2][i] = 2;
+		}
+	}
+	for (int i = 3; i < 101; i++) {
+		for (int j = 1; j < 10; j++) {
+			if (j == 1) {
+				dp[i][j] = dp[i - 2][j] + dp[i - 1][j + 1];
+			}
+			else if (j == 9) {
+				dp[i][j] = dp[i - 1][j - 1];
+			}
+			else {
+				dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j + 1];
+			}
+		}
+	}
+	for (int i = 1; i < 10; i++) {
+		sum += dp[n][i];
+	}
+	printf("%d", sum % 1000000000);
+	return 0;
+}
