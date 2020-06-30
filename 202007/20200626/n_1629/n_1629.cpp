4ea36d0c203bfa13bcd4c4728e7eb7525335adee
lis0517 <43354156+lis0517@users.noreply.github.com>
2020-06-30 20:17:18 +0900

    Update : 2020 06 30

---
+++ 202007/20200626/n_1629/n_1629.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+
+
+long long solution(int a, int b, int c)
+{
+    if (b == 1) return a % c;
+
+    long long value = solution(a, b / 2, c);
+    long long result = (b % 2 == 0) ? (value * value) % c : (value * value * a) % c;
+    return result;
+}
+
+int main()
+{
+    std::cout << solution(10, 11, 12);
+}
